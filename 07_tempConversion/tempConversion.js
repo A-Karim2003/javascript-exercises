b094d52f5106cd9365cb882a20c6cf13745b98b7
Abdoulkarim <17a.karim@gmail.com>
2025-03-27 15:04:41 +0000

    Completed temp convertion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  // °C = (°F - 32) × 5/9
+  return Number(((temp - 32) * (5 / 9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  // °F = (°C × 9/5) + 32
+  return Number(((temp * 9) / 5 + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
