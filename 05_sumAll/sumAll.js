4c9cbbca8a3203cc9f7dfa41c49823e41bcdbe30
Abdoulkarim <17a.karim@gmail.com>
2025-03-27 13:50:43 +0000

    Completed the sum all exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function (from, to) {
+  if (
+    from < 0 ||
+    to < 0 ||
+    typeof from !== "number" ||
+    typeof to !== "number" ||
+    !Number.isInteger(from) ||
+    !Number.isInteger(to)
+  )
+    return "ERROR";
 
+  min = Math.min(from, to);
+  max = Math.max(from, to);
+
+  sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
