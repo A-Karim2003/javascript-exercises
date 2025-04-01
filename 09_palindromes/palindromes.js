0592f38a4b9b5d4765f3f51409f6aa48904cade9
Abdoulkarim <17a.karim@gmail.com>
2025-04-01 19:23:07 +0100

    Completed palindromes using regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // Remove non-alphanumeric characters and convert to lowercase
 
+  const filteredStr = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  const reversedStr = [...filteredStr].reverse().join("");
+
+  return reversedStr === filteredStr;
 };
 
 // Do not edit below this line
